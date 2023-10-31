076d19e1f8d7dfa35fc511114d32e3f802ed15ed
Chad Crabtree <ccrabtree@apple.com>
2023-10-30 22:33:24 -0500

    no message

---
+++ words.js
@@ -0,0 +1,9 @@
+const myText = new AutoTyping({
+	id: "text", //Your HTML element id (string) - REQUIRED
+	typeText: [
+		`As the eerie moonlight cast shadows across the decrepit graveyard, the ancient MacBook Air sat on a moss-covered tombstone, its screen flickering to life with an otherworldly glow. A bony skeleton hand, long devoid of flesh and muscle, hovered over the keyboard, typing out an eerie message to the living world. \r\n"Welcome, mortal, to the realm of the digital undead," the skeletal fingers clacked on the keys, the sound echoing through the quiet night. "In this spectral domain, where code and cryptic secrets intertwine, I, the spectral scribe, shall unveil the cryptic knowledge hidden in the silicon depths of this ethereal device."\r\nThe screen displayed a ghostly text, as if etched in spectral fire. "Beware, for the boundary between the living and the dead is thin on this hallowed night. Seek answers to the mysteries of technology, and perhaps you shall unlock the secrets that lie beyond the grave."\r\nThe skeleton hand continued its eerie dance on the keyboard, typing out arcane symbols and cryptic incantations. It was as though the MacBook Air had become a conduit for the ancient wisdom of the digital afterlife, sharing its spectral knowledge with those who dared to venture into the computer graveyard on this haunted Halloween night.\r\nAs the final keystrokes echoed into the darkness, the screen displayed one last message: "Embrace the intersection of the living and the digital departed, for in this union, you may find the true essence of the Halloween spirit. Happy Haunting!"`,
+	],
+	typeRandom: true,
+	cursorColor: "#ccc",
+	textColor: "#fff",
+}).init();
